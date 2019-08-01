367e484b98350a102dc0c2672491c34b021befa6
Phil Shin <49408402+PBShin@users.noreply.github.com>
2019-08-01 16:38:13 -0500

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+var server = require('../server.js');
+
+class Form extends React.Component {
+    render () {
+        return (
+            <div>
+                <fieldset>
+                    <form action="/" method="post">
+                        <input name="city" type="text" placeholder="Enter a City" required></input>
+                        <input type="submit" value="Get Weather"></input>
+                    </form>
+                </fieldset>
+                <div>
+                    <h1>It is {this.props.weather} degrees Celsius!</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    consructor(props) {
+        super(props);
+        this.state = {
+            city=server.weather
+        }
+    }
+
+    render () {
+        return (
+            <Form city={this.state.city}/>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
+const request= require('request');
+const apiKey = 'ca2c8e9e8cde76ff7f239c77d8a47531';
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
 })
+
+app.post('/', function (req, res) {
+    var city = req.body.city;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+})
+
+request(url, function(err, res, body) {
+    if (err) {
+        res.render('index.html');
+        let weather= null;
+        let error = 'Error, please try again';
+    } else {
+        let weather = JSON.parse(body);
+        if (weather.main == undefined) {
+            res.render('index.html');
+            let weather = null;
+            let error = 'Error, please try again';
+        } else {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('index.html');
+            let weather = weatherText;
+            let error = null;
+        }
+    }
+})
+
+exports.weather = weather;
+exports.error = error;
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
